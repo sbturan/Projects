23724e71b07e1e1f942a4e750d58e09ac1190773
seckin <sb_turan@hotmail.com>
2016-09-16 03:06:40 -0400

    best

--- Leetcode/src/BasicCalculator.java
+++ Leetcode/src/BasicCalculator.java
@@ -7,90 +7,31 @@ public class BasicCalculator {
 	}
 
 	public int calculate(String s) {
-		s = s.replace(" ", "");
-		if (!s.contains("+") && !s.contains("-"))
-			return Integer.valueOf(s.replace("(", "").replace(")", ""));
-
-		boolean containParanthesis = true;
-		while (containParanthesis) {
-			int i = 0;
-			int start = 0;
-			while (i < s.length() && s.charAt(i) != ')') {
-				if (s.charAt(i) == '(') {
-					start = i + 1;
+		int len = s.length(), sign = 1, result = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		for (int i = 0; i < len; i++) {
+			if (Character.isDigit(s.charAt(i))) {
+				int sum = s.charAt(i) - '0';
+				while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
+					sum = sum * 10 + s.charAt(i + 1) - '0';
+					i++;
 				}
-				i++;
+				result += sum * sign;
+			} else if (s.charAt(i) == '+')
+				sign = 1;
+			else if (s.charAt(i) == '-')
+				sign = -1;
+			else if (s.charAt(i) == '(') {
+				stack.push(result);
+				stack.push(sign);
+				result = 0;
+				sign = 1;
+			} else if (s.charAt(i) == ')') {
+				result = result * stack.pop() + stack.pop();
 			}
-			if (i == s.length())
-				containParanthesis = false;
-			else {
-				s=getTotalBetween(start, i - 1, s);
-			}
-		}
-		
-		s=getTotalBetween(0, s.length()-1, s);
 
-		return Integer.valueOf(s);
+		}
+		return result;
 	}
 
-	private String getTotalBetween(int start, int end, String s) {
-		Integer total = 0;
-		int currentSign = 0;
-		if(s.charAt(start)=='-') {
-			currentSign=1;
-			start++;
-		}
-		
-		Integer cur = start;
-		while (true) {
-			int number = getNumber(cur, s);
-			while(cur<s.length()&&Character.isDigit(s.charAt(cur))) cur++;
-			if (currentSign == 0)
-				total += number;
-			else
-				total -= number;
-			if (cur >= end)
-				break;
-			char c = s.charAt(cur);
-			if (c == '+')
-				currentSign = 0;
-			else
-				currentSign = 1;
-			cur++;
-		}
-		String result="";
-		if(start!=0)result = s.substring(0,start-1);
-		
-		result += total.toString();
-		if (end < s.length() - 2) {
-			result = result + s.substring(end + 2);
-		}
-		int index=0;
-		while(index<result.length()-1){
-			char firstChar = result.charAt(index);
-			char secondChar = result.charAt(index+1);
-			if((firstChar=='+'&&secondChar=='+')||
-					(firstChar=='+'&&secondChar=='-')||
-					(firstChar=='-'&&secondChar=='+')||
-					(firstChar=='-'&&secondChar=='-')){
-				if(firstChar!=secondChar){
-					result=result.substring(0,index)+"-"+result.substring(index+2);
-				}else{
-					result=result.substring(0,index)+"+"+result.substring(index+2);
-				}
-			}
-			index++;
-		}
-	
-   return result;
-	}
-
-	private int getNumber(Integer i, String s) {
-		int k = i;
-		while (k < s.length() && Character.isDigit(s.charAt(k))) {
-			k++;
-		}
-
-		return Integer.valueOf(s.substring(i, k));
-	}
 }
